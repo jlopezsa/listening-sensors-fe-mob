d058058c106b8c9d6ac7cb4fc048989b09867857
Julian Lopez <jlopezsa@gmail.com>
2022-05-12 16:41:56 -0500

    feat: Paste code in firebase.js file

---
+++ database/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyANoaEsNHHUEbUomvFtLy62WX7moK78FB8',
+  authDomain: 'listeningsensors.firebaseapp.com',
+  databaseURL: 'https://listeningsensors-default-rtdb.firebaseio.com',
+  projectId: 'listeningsensors',
+  storageBucket: 'listeningsensors.appspot.com',
+  messagingSenderId: '634491630956',
+  appId: '1:634491630956:web:f6d37d278e93993c8daf49',
+  measurementId: 'G-E2B396DSM5',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
