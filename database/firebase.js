0ad9e2bd0805e2bc91bf37a882c543e2f61df6ef
Julian Lopez <jlopezsa@gmail.com>
2022-05-12 17:02:22 -0500

    feat: import firestore with a comment

--- database/firebase.js
+++ database/firebase.js
@@ -4,7 +4,8 @@ import { getAnalytics } from 'firebase/analytics';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+// import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import { getFirestore } from 'firebase/firestore/lite';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
