e223ffc3d098af6a31d668363cb5a031545962a8
Julian Lopez <jlopezsa@gmail.com>
2022-05-12 17:01:51 -0500

    feat: import firestore

--- database/firebase.js
+++ database/firebase.js
@@ -4,6 +4,8 @@ import { getAnalytics } from 'firebase/analytics';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -20,3 +22,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+export {
+  app, analytics, db,
+};
