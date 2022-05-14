34272835f4dc252e237a35589446610334d21fe9
Julian Lopez <jlopezsa@gmail.com>
2022-05-14 13:09:42 -0500

    feat: et data from gyroscope but the component in not used in the app

--- App.js
+++ App.js
@@ -3,15 +3,15 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import AccelerometerSensor from './components/Accelerometer/Accelerometer';
 import GyroscopeSensor from './components/Gyroscope/Gyroscope';
-import LocationComponent from './components/Location/LocationComponent'
+// import LocationComponent from './components/Location/LocationComponent'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Enable sensors:</Text>
-      {/* <AccelerometerSensor /> */}
-      {/* <GyroscopeSensor /> */}
-      <LocationComponent />
+      <AccelerometerSensor />
+      <GyroscopeSensor />
+      {/* <LocationComponent /> */}
       <StatusBar style="auto" />
     </View>
   );
